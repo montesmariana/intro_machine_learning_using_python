f7a2a44b6491031df405001334db13d2ebaee7d0
Elmo.Geeraerts <elmo.geeraerts@student.kuleuven.be>
2023-05-21 10:57:41 +0200

    Update vendor_data

--- vendor_data.py
+++ vendor_data.py
@@ -7,9 +7,9 @@ The script contains three classes:
        It has 3 class arguments ´ProjectName´, representing the project name and ´SourceLang´, the source language the translators will be translating from and ´CatTools´, the possible Cat Tools the translator can use. 
        It takes 5 arguments:
             - ´VendorName´: The first and last name of the vendor.
-            - ´VendorMail´: The vendor's email address.
             - ´TargLang´: the language the translators will be translating into.
             - ´WordRate´: the translator's word rate in Euro.
+            - ´VendorMail´: The vendor's email address.
             -  ´CatTool´: the vendor's preferred cat tool.
     2. The subclass ´PrefVendor´, for the preferred vendors. They are assigned the status "Preferred".
     3. The subclass ´BackupVendor´ for the back-up vendors. They are assigned the status "Back-up"
@@ -17,7 +17,6 @@ The script contains three classes:
 import argparse
 import re
 
-# write here your function and class definitions
 class VendorData:
     ProjectName = "Toyota MM24" #Class attribute no.1, the project the vendors will be working on
     SourceLang = "English" #Class attribute no.2, the source language
@@ -33,13 +32,14 @@ class VendorData:
             "CatTool" (str): Vendor's preferred CAT-tool
             """
         self.VendorName = VendorName
-        self.VendorMail = VendorMail
         self.TargLang = TargLang
         self.WordRate = WordRate
+        self.VendorMail = VendorMail
         self.CatTool = CatTool
         
         regex_mail = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$" #regex used to validate email
-        
+            
+
         """
         Exceptions:
             VendorName should be a string, if not a TypeError is raised.
@@ -61,6 +61,8 @@ class VendorData:
         if WordRate:
             if WordRate > 0.15:
                 raise ValueError("This vendor is too expensive, consider another one.")
+        elif WordRate == 0.00:
+            raise ValueError("Word rate cannot be 0.00.")
         else:
             self.WordRate = WordRate
         if type(CatTool) != str:
@@ -78,17 +80,32 @@ class VendorData:
             self.CatTool = CatTool
         else:
             raise ValueError("This CAT tool is not valid. Run 'VendorData.CatTools' to check options.")
+            
+    def set_mail(self,mail):
+        regex_mail = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$" #regex used to validate email
+        if (re.search(regex_mail,mail)):
+            self.VendorMail = mail
+        else:
+            raise ValueError("Please insert a valid email address.")
+
         
 class PrefVendor(VendorData):
     Preferred = True
     """ Class Attribute:
             Preffered (bool.) by default set to "True" indicating this is a Preferred Vendor, if "False" it is a back-up vendor."""
     
-    def __init__(self, VendorName, VendorMail, TargLang, WordRate, CatTool):
-        super().__init__(VendorName, VendorMail, TargLang, WordRate, CatTool)
+    def __init__(self, VendorName, TargLang, WordRate, VendorMail, CatTool):
+        super().__init__(VendorName, TargLang, WordRate, VendorMail, CatTool)
+
 
 # don't forget to properly dcument them with docstrings!!
 
 if __name__ == "__main__":
-    # write here what happens if the code is run instead of imported
-    # include argparse code!
+    parser = argparse.ArgumentParser("docstring")
+    parser.add_argument("VendorName", type=str, help="Vendor's name")
+    parser.add_argument("TargLang", type=str, help="Language the vendor will be translating into")
+    parser.add_argument("--WordRate", type=float, help="The vendor's word rate in Euro")
+    parser.add_argument("--VendorMail", type=str, help="The vendor's email address")
+    parser.add_argument("--CatTool", type=str, help="The CAT-tool the vendor wants to use")
+    args = parser.parse_args()
+    Vndr = VendorData(args.VendorName, args.TargLang, args.WordRate, args.VendorMail, args.CatTool)
